1faaf3eff43060f34a64662e9dd26082263f3744
Miguel Diganchi <migueldiganchi@gmail.com>
2019-05-24 00:56:52 -0300

    foldings data moved to an isolated file

--- src/index.js
+++ src/index.js
@@ -8,53 +8,10 @@ import Developer from "./components/Developer/Developer";
 
 import "./styles.css";
 
-const appTitle = "Some rules in life";
+// api
+import foldings from "./io/foldings";
 
-const foldings = [
-  {
-    id: 1,
-    title: "Before you pray",
-    content: 
-      <div>
-        <p>Believe</p>
-      </div>
-  }, {
-    id: 2,
-    title: "Before speak",
-    content: 
-      <div>
-        <p>Listen</p>
-      </div>
-  }, {
-    id: 3,
-    title: "Before you spend",
-    content: 
-      <div>
-        <p>Earn</p>
-      </div>
-  }, {
-    id: 4,
-    title: "Before you write",
-    content: 
-      <div>
-        <p>Think</p>
-      </div>
-  }, {
-    id: 5,
-    title: "Before you quit",
-    content: 
-      <div>
-        <p>Try</p>
-      </div>
-  }, {
-    id: 6,
-    title: "Before die",
-    content: 
-      <div>
-        <p>Live</p>
-      </div>
-  }
-];
+const appTitle = "Some rules in life";
 
 const developer = {
   name: "Miguel Diganchi",
---
+++ src/io/foldings.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default [
+    {
+      id: 1,
+      title: "Before you pray",
+      content: 
+        <div>
+          <p>Believe</p>
+        </div>
+    }, {
+      id: 2,
+      title: "Before speak",
+      content: 
+        <div>
+          <p>Listen</p>
+        </div>
+    }, {
+      id: 3,
+      title: "Before you spend",
+      content: 
+        <div>
+          <p>Earn</p>
+        </div>
+    }, {
+      id: 4,
+      title: "Before you write",
+      content: 
+        <div>
+          <p>Think</p>
+        </div>
+    }, {
+      id: 5,
+      title: "Before you quit",
+      content: 
+        <div>
+          <p>Try</p>
+        </div>
+    }, {
+      id: 6,
+      title: "Before die",
+      content: 
+        <div>
+          <p>Live</p>
+        </div>
+    }
+  ];
